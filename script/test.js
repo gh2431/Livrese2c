867fbf58a003dd5cbd4f15c48806c92a6b2c67cb
gh2431 <helygael16@gmail.com>
2024-11-19 15:29:18 +0100

    javascrit

---
+++ script/test.js
@@ -0,0 +1,11 @@
+//console.log("hello world!");//
+
+//let userName = "pne2c"; //string
+//let age = 24; //int
+//let size = 1.75; //float
+//let isCool = true; //bool
+
+let a = 15
+let b = 35
+
+console.log(a+b, a/b, a-b, a*b, a%b)
