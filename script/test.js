ebb61a3df4dd672d46fcb7a9082e846b559813a4
gh2431 <helygael16@gmail.com>
2024-11-25 16:17:47 +0100

    jv 2

--- script/test.js
+++ script/test.js
@@ -5,7 +5,45 @@
 //let size = 1.75; //float
 //let isCool = true; //bool
 
-let a = 15
-let b = 35
+let resultathtml = document.querySelector('#resultat');
+/*console.log(resultathtml);*/
 
-console.log(a+b, a/b, a-b, a*b, a%b)
+let nombre1 = parseInt(prompt("donnez un premier nombre"))
+
+while(isNaN(nombre1)) {
+    nombre1 = prompt("Il faut écrire un nombre")
+}
+
+/*if(isNaN(nombre1)){
+    console.log("Il faut écrire un chiffre")
+} else { */
+
+let nombre2 = parseInt(prompt("Donnez un deuxième nombre"))
+
+while(isNaN(nombre2)) {
+    nombre2 = prompt("Il faut écrire un nombre")
+}
+
+/*if(isNaN(nombre2)){
+    console.log("Il faut écrire un chiffre")
+} else { */
+
+let resultat = nombre1 + nombre2;
+let messageaddition = "<p>Le resultat de l'adittion de "+nombre1+" et de "+nombre2+" est "+resultat+"</p>";
+resultathtml.innerHTML = messageaddition;
+
+resultat = nombre1 - nombre2;
+let messagesoustraction =`<p>Le résultat de la soustraction de ${nombre1} et de ${nombre2} est ${resultat}</p>`;
+resultathtml.innerHTML += messagesoustraction;
+
+resultat = nombre1 * nombre2;
+let messagemultiplications = `<p>Le résultat de la multiplications de ${nombre1} et de ${nombre2} est ${resultat}</p>`;
+resultathtml.innerHTML += messagemultiplications;
+
+resultat = nombre1 / nombre2;
+let messagedivisions = `<p>Le résultat de la divisions de ${nombre1} et de ${nombre2} est ${resultat}</p>`;
+resultathtml.innerHTML += messagedivisions;
+
+/*for(let i = 1; i<=10; i++) {
+    console.log(`${i} - bonjour`)
+}*/
